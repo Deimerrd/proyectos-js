dcbc9fbdd0fa0017574368cb25780c6287f265f7
Deimerd <deimerblandonmc@gmail.com>
2024-07-29 11:26:32 -0500

    cambio taller2

--- 02-fylo-data-storage-component/java.js
+++ 02-fylo-data-storage-component/java.js
@@ -1,8 +1,8 @@
-const price = document.querySelector('#price');
-const output = document.querySelector('.price-output');
-const totalOcupado = document.querySelector('.totalOcupado');
+const price = document.querySelector("#price");
+const output = document.querySelector(".price-output");
+const totalOcupado = document.querySelector(".totalOcupado");
 
-price.addEventListener('input', function () {
+price.addEventListener("input", function () {
   output.textContent = 1000 - parseInt(price.value);
   totalOcupado.textContent = price.value;
 });
