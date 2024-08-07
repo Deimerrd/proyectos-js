b1597e68e80036cd17fd60e62583cb9127896dac
Deimerd <deimerblandonmc@gmail.com>
2024-08-06 23:25:30 -0500

    header-terminado

---
+++ 05-fylo-landing-page-with-two-column-layout/validarformulario.js
@@ -0,0 +1,12 @@
+function validaremail() {
+  var email = document.getElementById("email").value;
+  var error = document.getElementById("error");
+  var expresion =
+    /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
+
+  if (email.match(expresion)) {
+    alert("La dirección de email " + email + " es correcta!.");
+  } else {
+    error.innerHTML = "Check your email please";
+  }
+}
