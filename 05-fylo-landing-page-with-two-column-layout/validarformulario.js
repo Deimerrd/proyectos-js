8b85a31caec40b346e5cdf5e8266b87ee534776a
Deimerd <deimerblandonmc@gmail.com>
2024-08-08 22:36:25 -0500

    cambio responsibe

--- 05-fylo-landing-page-with-two-column-layout/validarformulario.js
+++ 05-fylo-landing-page-with-two-column-layout/validarformulario.js
@@ -10,3 +10,16 @@ function validaremail() {
     error.innerHTML = "Check your email please";
   }
 }
+
+function validaremailfooter() {
+  var email = document.getElementById("email-f").value;
+  var error = document.getElementById("error-f");
+  var expresion =
+    /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
+
+  if (email.match(expresion)) {
+    alert("La dirección de email " + email + " es correcta!.");
+  } else {
+    error.innerHTML = "Please check your email";
+  }
+}
