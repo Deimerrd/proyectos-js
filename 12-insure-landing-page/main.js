22ec5fdcfaba419b60066bb7472399191ae289c5
Deimerd <deimerblandonmc@gmail.com>
2024-11-06 19:30:58 -0500

    eliminado-archivojs

--- 12-insure-landing-page/main.js
+++
@@ -1,8 +0,0 @@
-let menuHamburguesa = document.querySelector(".nav__logo");
-
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-menuHamburguesa.addEventListener("click", () => {
-  let menu = document.querySelector(".list");
-  menu.classList.toggle("list--show");
-});
