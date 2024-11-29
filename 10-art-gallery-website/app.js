8ab00f503faefe2f2110ced60bd7d6e8c11d64ff
Deimerd <deimerblandonmc@gmail.com>
2024-11-28 21:43:44 -0500

    j

---
+++ 10-art-gallery-website/app.js
@@ -0,0 +1,5 @@
+const titulo = document.getElementById("title");
+titulo.innerHTML = "gallery paragraph";
+
+const paragraph = document.getElementById("paragraph");
+paragraph.innerHTML = "texto paragraph actualizado";
