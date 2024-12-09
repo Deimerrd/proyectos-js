8498a2cf6e1ebee467c31804e379a88c7e9fe823
Deimerd <deimerblandonmc@gmail.com>
2024-12-09 15:19:13 -0500

    formulario validando

--- 15-maker-pre-launch-landing-page/index.js
+++ 15-maker-pre-launch-landing-page/index.js
@@ -1,5 +1,37 @@
-const form = document.getElementById("form");
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let email = Document.getElementById("txt__email").value;
-});
+function validaremail() {
+  function validarfor() {
+    function validaVacio(valor) {
+      valor = valor.replace("&nbsp;", "");
+      valor = valor == undefined ? "" : valor;
+      if (!valor || 0 === valor.trim().length) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    var email = document.getElementById("txt__email").value;
+    var error = document.getElementById("error");
+    let expresion =
+      /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
+
+    if (!expr.test(email)) {
+      //COMPRUEBA MAIL
+      error.innerHTML = " Oops! That doesn’t look like an email address";
+
+      return false;
+    }
+
+    if (email.match(expresion)) {
+      alert("La dirección de email " + email + " es correcta!.");
+
+      document.getElementById("email").style.borderColor = "#fff";
+    }
+    if (validaVacio(email.value)) {
+      //COMPRUEBA CAMPOS VACIOS
+      error.innerHTML = "Oops! Please add your email";
+
+      return false;
+    }
+    return true;
+  }
+}
