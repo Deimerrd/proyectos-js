60c3239a9457bd182e147c5176ca958bd09da663
Deimerd <deimerblandonmc@gmail.com>
2024-12-09 19:28:13 -0500

    javascript solucion

--- 15-maker-pre-launch-landing-page/index.js
+++ 15-maker-pre-launch-landing-page/index.js
@@ -1,37 +1,20 @@
 function validaremail() {
-  function validarfor() {
-    function validaVacio(valor) {
-      valor = valor.replace("&nbsp;", "");
-      valor = valor == undefined ? "" : valor;
-      if (!valor || 0 === valor.trim().length) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    var email = document.getElementById("txt__email").value;
-    var error = document.getElementById("error");
-    let expresion =
-      /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
+  const email = document.getElementById("txt__email").value;
+  const error = document.getElementById("error");
+  let expresion =
+    /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
 
-    if (!expr.test(email)) {
-      //COMPRUEBA MAIL
-      error.innerHTML = " Oops! That doesn’t look like an email address";
-
-      return false;
-    }
-
-    if (email.match(expresion)) {
-      alert("La dirección de email " + email + " es correcta!.");
-
-      document.getElementById("email").style.borderColor = "#fff";
-    }
-    if (validaVacio(email.value)) {
-      //COMPRUEBA CAMPOS VACIOS
-      error.innerHTML = "Oops! Please add your email";
-
-      return false;
-    }
+  if (email.match(expresion)) {
+    document.getElementById("txt__email").value = "";
+    error.innerHTML = "";
     return true;
+  }
+
+  if (!expresion.test(email) && email > "0") {
+    error.innerHTML = " Oops! That doesn’t look like an email address";
+    error.style.display = "block";
+  } else {
+    error.innerHTML = " Oops! Please add your email";
+    error.style.display = "block";
   }
 }
