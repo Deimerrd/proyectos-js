2cff213a349f5f2f5f66533355df71fd8b47b4c4
Deimerd <deimerblandonmc@gmail.com>
2024-12-28 20:21:35 -0500

    creando-menu

--- 16-blogr-landing-page/index.js
+++ 16-blogr-landing-page/index.js
@@ -2,6 +2,9 @@ const menuButton = document.getElementById("menu__button");
 const navbar = document.getElementById("nav");
 const closeMenu = document.querySelector(".close");
 const openMenu = document.querySelector(".open");
+const openlist = document.getElementById("list-show");
+const list = document.querySelectorAll("ul__list");
+const toggleArrow = document.getElementById("arrow");
 
 menuButton.addEventListener("click", () => {
   if (navbar.classList.contains("open")) {
@@ -13,4 +16,14 @@ menuButton.addEventListener("click", () => {
     closeMenu.style.display = "block";
     openMenu.style.display = "none";
   }
+});
+
+const toggleDropdown = function () {
+  list.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow");
+};
+
+openlist.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown();
 });
