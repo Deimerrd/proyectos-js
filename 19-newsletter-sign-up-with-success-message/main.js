9057670029d72b7aaa77506cb04a0f3dd8f6418c
Deimerd <deimerblandonmc@gmail.com>
2025-05-11 01:08:17 -0500

    problema-resuelto

--- 19-newsletter-sign-up-with-success-message/main.js
+++ 19-newsletter-sign-up-with-success-message/main.js
@@ -9,15 +9,18 @@ function validaremaill() {
 
     document.getElementById("email").style.borderColor = "#fff";
   } else {
-    error.innerHTML = "Error, please check your email";
+    error.innerHTML = "Valid email required";
+    error.style.color = " #FF6155";
 
-    document.getElementById("email").style.borderColor = "#EF4877";
+    document.getElementById("email").style.fontSize = "16px";
   }
 }
 
 /*document.getElementById("btn-email").addEventListener("click", function () {
   const Email = document.getElementById("email").value;
   const errorSpan = document.getElementById("emailError");
+
+  color: var(--Vermellion, #FF6155);
 
   if (esCorreoValido(Email)) {
     // Redirige a la nueva página
