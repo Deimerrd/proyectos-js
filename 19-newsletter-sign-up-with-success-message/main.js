18cbeb7a2b5f6741aeddc3362e82fd04f6cae04a
Deimerd <deimerblandonmc@gmail.com>
2025-05-10 19:08:16 -0500

    comenzandojsca

--- 19-newsletter-sign-up-with-success-message/main.js
+++ 19-newsletter-sign-up-with-success-message/main.js
@@ -1,23 +1,10 @@
-function validarYRedireccionar() {
-  const email = document.getElementById("email").value;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (emailRegex.test(email)) {
-    window.location.href = "suscrito.html";
-  } else {
-    alert("El correo electrónico no es válido.");
-  }
-}
-
-/*document.getElementById("btn-email").addEventListener("click", function () {
+document.getElementById("btn-email").addEventListener("click", function () {
   const Email = document.getElementById("email").value;
   const errorSpan = document.getElementById("emailError");
 
   if (esCorreoValido(Email)) {
     // Redirige a la nueva página
-    window.location.replace("suscrito.html"); // o 'ruta/a/la/otra/pagina.html'
-    // O también:
-    //
+    self.location = "suscrito.html";
   } else {
     errorSpan.innerHTML = "Valid email required"; // plain javascript
   }
@@ -27,4 +14,4 @@ function esCorreoValido(Email) {
   // Implementa la validación del correo (ejemplo simple)
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(Email);
-}*/
+}
