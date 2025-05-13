fec8432f725d9fb3e16940098d1215925fde2932
Deimerd <deimerblandonmc@gmail.com>
2025-05-12 20:21:21 -0500

    proyecto19realizado

--- 19-newsletter-sign-up-with-success-message/main.js
+++ 19-newsletter-sign-up-with-success-message/main.js
@@ -15,24 +15,3 @@ function validaremaill() {
     document.getElementById("email").style.fontSize = "16px";
   }
 }
-
-/*document.getElementById("btn-email").addEventListener("click", function () {
-  const Email = document.getElementById("email").value;
-  const errorSpan = document.getElementById("emailError");
-
-  color: var(--Vermellion, #FF6155);
-
-  if (esCorreoValido(Email)) {
-    // Redirige a la nueva página
-    window.location.href = "suscrito.html";
-  } else {
-    errorSpan.innerHTML = "Valid email required"; // plain javascript
-  }
-});
-
-function esCorreoValido(Email) {
-  // Implementa la validación del correo (ejemplo simple)
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(Email);
-}
-*/
