aef390237f29524e53b30bbebb520aa02587d86d
Deimerd <deimerblandonmc@gmail.com>
2025-07-16 11:49:57 -0500

    nav

--- 23-Social-Media-Dashboard-With-Theme-Switcher/src/components/Navbar/index.jsx
+++ 23-Social-Media-Dashboard-With-Theme-Switcher/src/components/Navbar/index.jsx
@@ -1,8 +1,19 @@
 const Navbar = () => {
   return (
-    <div>
-      <p>Navbar</p>
-      <input placeholder="busca tu evento favorito" />
+    <div className="content">
+      <nav
+        className="nav"
+        style={{ backgroundColor: "#f7f9ff", color: "#1d1f29" }}
+      >
+        <h1 className="nav__title">Social Media Dashboard</h1>
+        <p className="nav__subtitle">Total Followers: 23,004</p>
+
+        <hr className="divider" />
+        <div className="dark-mode-toggle">
+          <label>Dark Mode</label>
+          <input type="checkbox" />
+        </div>
+      </nav>
     </div>
   );
 };
